d59eb2847e4eccd901f40409bdb15783208f0952
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-11-19 09:39:08 +0100

    Add Fody

--- src/WSDLVersionInfo/VersionInfo.cs
+++ src/WSDLVersionInfo/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace WSDLVersionInfo
 {
     public static class VersionInfo
     {
-        public const string AssemblyCopyright = "Copyright © Stef Heyenrath 2010 - 2017";
-        public const string Version = "0.0.0.13";
+        public const string AssemblyCopyright = "Copyright © Stef Heyenrath 2010 - 2018";
+        public const string Version = "0.0.0.14";
     }
 }
